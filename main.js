50565a450389bfcf1b7e2977749393cdf1400478
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-25 21:50:52 -0300

    main.js- criar lanche

---
+++ main.js
@@ -0,0 +1,37 @@
+/**
+ * processo principal
+ * estudo do CRUD - aividade pratica (mongodb)
+ */
+
+//importação do modulo de conexão (database.js)
+const { conectar, desconectar } = require(database.js)
+
+//importaçao do modelo de dados (hambugueres/produtos)
+const lanchesModel = require('./src/modelssrc/models/lanches.js')
+
+//importaçao do pacote "string-similarity"
+const stringsimilarity = require('string-similarity')
+
+//CRUD- CREATE- 
+
+const criarLanche = async (nomeLan, precoLan, descricaoLan, imagemLan) => {
+    try {
+        const novoLanche = new lanchesModel(
+            {
+            nomeLanche: nomeLan,
+            precoLanche: precoLan,
+            descricaoLanche: descricaoLan,
+            imagemLanche: imagemLan
+            }
+        )
+        //a linha abaixo salva os dados do cliente nos bancos de dados
+        await novoLanche.save()
+        console.log("lanche criado com sucesso")
+    } catch (error) {
+      if (error.code = 11000) {
+        console.log(`ERRO: este lanche ja existe`)
+      }else {
+        console.log(error)
+      }   
+    }
+}
