32a95cba8d086de69819ada3479dae3f343209f4
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-25 21:40:15 -0300

    Modelo de dados Lanches

---
+++ src/models/lanches.js
@@ -0,0 +1,28 @@
+/**
+ * Modelo de dados (coleção - MongoDB)
+ * Hamburgueres
+ */
+
+// Importando biblioteca do mongoose
+const {model, Schema } = require('mongoose')
+
+// Criando a estrutura de dados hamburguers para o banco de dados
+const LanchesSchema = new Schema({
+    nomeLanche:{
+        type: String,
+    },
+    precoLanche:{
+        type: Number,
+    },
+    descricaoLanche:{
+        type: String,
+    },
+    imagemLanche:{
+        type: String,
+    }
+}, {versionKey: false})
+
+// Importação de modelo de dados
+// Nome da coleção: lanches
+
+module.exports = model('lanches', LanchesSchema)
