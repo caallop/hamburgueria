341b594b0c2eb56c86e77d4989c20df3c24af0f6
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-27 22:27:27 -0300

    MongoDB

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const { conectar, desconectar } = require('./database.js')
 const lanchesModel = require('./src/models/lanches.js')
 
 //importaçao do pacote "string-similarity"
-const stringsimilarity = require('string-similarity')
+const stringSimilarity = require('string-similarity')
 
 //CRUD- CREATE- 
 
@@ -36,23 +36,119 @@ const criarLanche = async (nomeLan, precoLan, descricaoLan, imagemLan) => {
     }
 }
 
+//funçao para listar todos os lanches
+const listarLanches = async () => {
+    try {
+        //a linha abaixo lista todos os clientes
+        const lanche = await lanchesModel.find()
+        console.log(lanche)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Funçao para buscar um lanche especifico pelo nome
+const buscarLanche = async (nomeLan) => {
+    try {
+        // find() buscar 
+        // nomeCliente: new RegExp(nome, 'i') filtro pelo nome (partes que contenham(expressão regular))
+        // 'i' insensitive (ignorar letras maiusculas ou minúsculas) 
+        const lanche = await lanchesModel.find(
+            {
+
+                nomeLanche: new RegExp(nomeLan, 'i')
+            }
+        )
+        // Calcular a similaridade entre os nomes retornados e o nome pesquisado
+        const nomeLanches = lanche.map(lanche => lanche.nomeLanche)
+
+        // validação (se não existir o cliente pesquisado)
+        if (nomeLanches.length === 0) {
+            console.log("Cliente não cadastrado")
+        } else {
+            const match = stringSimilarity.findBestMatch(nomeLan, nomeLanches)
+
+            // cliente com melhor similaridade
+            const melhorLanche = lanche.find(lanche => lanche.nomeLanche === match.bestMatch.target)
+
+            console.log(melhorLanche)
+        }
+        //console.log(cliente)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+const atualizarLanche = async (id, nomeLan, descricaoLan, precoLan) => {
+    try {
+        const lanche = await lanchesModel.findByIdAndUpdate(
+            id,
+            {
+                nomeLanche: nomeLan,
+                descricaoLanche: descricaoLan,
+                precoLanche: precoLan
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        if (!lanche) {
+            console.log("Cliente não encontrado")
+        } else {
+            console.log("Dados do clinte alterados com sucesso")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const deletarLanche = async (id) => {
+    try {
+        const lanche = await lanchesModel.findByIdAndDelete(id)
+
+        if (!lanche) {
+            console.log("Cliente não encontrado")
+        } else {
+            console.log("Cliente deletado com sucesso")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
 const app = async () => {
 
     //tentativa de conexao
     await conectar()
 
     // Cadastrar lanche - 1
-    await criarLanche('Lanche 1', '12.54', 'Pão,  carne - 150g, queijo e muito amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 2', '15.91', 'Pão,  carne - 180g, queijo e muito mais amor', 'img/lanchebom2.png',)
-    await criarLanche('Lanche 3', '14.49', 'Pão,  carne - 150g, queijo, alface e pouco amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 4', '12.58', 'Pão,  carne - 150g, queijo, alface, tomate e muito amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 5', '19.57', 'Pão,  carne - 180g, queijo, alface, tomate, picles e MUITO amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 6', '12.56', 'Pão,  carne de cordeiro - 150g, queijo, alface e muito amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 7', '54.91', 'Pão,  carne vegana - 150g, queijo vegano, alface vegano, amor vegano', 'img/lanchebom.png',)
-    await criarLanche('Lanche 8', '27.52', 'Pão,  2 carnes - 150g, queijo, alface, tomate, picles, cebola, bacon, cebola caramelizada, onion ring, fé, mais hamburguer e muito amor', 'img/lanchebom.png',)
-    await criarLanche('Lanche 9', '190.84', 'Pão,  5 carnes - 180g, queijo, feijao tropeiro, açai, calabresa, calabreso, ovo, queijo, cheddar, molhos (todos), alegria, tristeza, mulher do corno, e muito amor', 'img/lanchebom.png',)
+    // await criarLanche('simples', '12.54', 'Pão,  carne - 150g, queijo e muito amor', 'img/lanchebom.png',)
+    // await criarLanche('x amor', '15.91', 'Pão,  carne - 180g, queijo e muito mais amor', 'img/lanchebom2.png',)
+    // await criarLanche('x burguer love', '14.49', 'Pão,  carne - 150g, queijo, alface e pouco amor', 'img/lanchebom.png',)
+    // await criarLanche('mc cheese', '12.58', 'Pão,  carne - 150g, queijo, alface, tomate e muito amor', 'img/lanchebom.png',)
+    // await criarLanche('x-picles', '19.57', 'Pão,  carne - 180g, queijo, alface, tomate, picles e MUITO amor', 'img/lanchebom.png',)
+    // await criarLanche('cordeirinho', '12.56', 'Pão,  carne de cordeiro - 150g, queijo, alface e muito amor', 'img/lanchebom.png',)
+    // await criarLanche('pink money', '54.91', 'Pão,  carne vegana - 150g, queijo vegano, alface vegano, amor vegano', 'img/lanchebom.png',)
+    // await criarLanche('x tudinho', '27.52', 'Pão,  2 carnes - 150g, queijo, alface, tomate, picles, cebola, bacon, cebola caramelizada, onion ring, fé, mais hamburguer e muito amor', 'img/lanchebom.png',)
+    // await criarLanche('x tudao', '190.84', 'Pão,  5 carnes - 180g, queijo, feijao tropeiro, açai, calabresa, calabreso, ovo, queijo, cheddar, molhos (todos), alegria, tristeza, mulher do corno, e muito amor', 'img/lanchebom.png',)
 
+    //para buscar todos da lista
+    // await listarLanches()
 
+    //buscar um NOME especifico
+    //await buscarLanche("")
+
+    //Alterar os dados de um lanche por id
+    await atualizarLanche('67c0fd255dd568e274907829', 'vitor', 'lanche bao a dessa', '1')
+
+    //exlcluir os dados de um lanche por id
+    await deletarLanche('67c0fd255dd568e274907829')
 
     //tentatica de desconexao
     await desconectar()
